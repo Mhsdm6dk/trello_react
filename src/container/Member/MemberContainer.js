1c2b0aff5e93288c25baf751e066f4b067f0dc12
Nguyen Hanh <nguyenhanh3@telio.vn>
2022-06-02 22:26:43 +0700

    10h30

--- src/container/Member/MemberContainer.js
+++ src/container/Member/MemberContainer.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MemberComponent from './component/MemberComponent';
-
+import lodash from 'lodash';
+import axiosConfig from '../../share/axiosConfig';
+import { useQuery } from 'react-query';
 function MemberContainer(props) {
+    const [searchNameInput, setSearchNameInput] = useState('');
+    const [listSearchUser, setListSearchUser] = useState([]);
+    const [showSearch, setShowSearch] = useState(false);
+    const { data:listUser =[], isLoading } = useQuery('getListUser', () => axiosConfig.get('/api/user'));
+    const nhap = lodash.debounce(async (e) => {
+        if (e.target.value != '') {
+            const data = await axiosConfig.get(`/api/user/search?email=${e.target.value}`);
+            setListSearchUser(data)
+        }
+    }, 1000)
     return (
-        <MemberComponent/>
+        <MemberComponent nhap={nhap} listSearchUser={listSearchUser} listUser={listUser} showSearch={showSearch} setShowSearch={setShowSearch} searchNameInput={searchNameInput} setSearchNameInput={setSearchNameInput} />
     )
 }
 
--- src/container/Member/component/MemberComponent.js
+++ src/container/Member/component/MemberComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css';
 function MemberComponent(props) {
-    return (    
+    return (
         <div className={styles.memberPage}>
             <div className={styles.memberBox}>
                 <div className={styles.memberNavbarBox}>
@@ -17,10 +17,100 @@ function MemberComponent(props) {
                         Các thành viên Không gian làm việc (1)
                     </h2>
                     <p className={styles.memberBody_describes}>
-                    Các thành viên trong Không gian làm việc có thể xem và tham gia tất cả các bảng Không gian làm việc hiển thị và tạo ra các bảng mới trong Không gian làm việc.
+                        Các thành viên trong Không gian làm việc có thể xem và tham gia tất cả các bảng Không gian làm việc hiển thị và tạo ra các bảng mới trong Không gian làm việc.
                     </p>
                     <div className={styles.memberBody_line}>
-                        
+
+                    </div>
+                    <h2 className={styles.memberBody_heading}>
+                        Mời các thành viên tham gia cùng bạn
+                    </h2>
+
+                    <p className={styles.memberBody_describes}>
+                        Bất kỳ ai có liên kết duy nhất đều có thể tham gia Không gian làm việc này, với 3 bảng. Bạn sẽ bị tính phí cho mỗi thành viên được thêm vào. Bạn có thể tắt và tạo liên kết mới cho Không gian làm việc này bất kỳ lúc nào.
+                    </p>
+
+                    <div className={styles.memberBody_line}>
+
+                    </div>
+                    <div className={styles.searchBox}>
+                        <input className={styles.searchNameInput} placeholder="Lọc theo tên" />
+                        <div style={{ position: "relative" }}>
+                            <div className={styles.addMemberButton} onClick={
+                                () => props.setShowSearch(show => !show)
+                            }>
+                                <i className={"fa-solid fa-user-plus " + styles.addMemberIcon}></i>
+                                Mời các thành viên trong A.iSoft
+                            </div>
+                            {
+                                props.showSearch ? <div className={styles.resultBox}>
+                                    <div className={styles.resultBox_header}>
+                                        Thêm thành viên
+                                        <i className={"fa-solid fa-xmark " + styles.exitIcon} onClick={() => props.setShowSearch(false)}></i>
+
+                                    </div>
+                                    <div className={styles.resultBox_body}>
+                                        <div className={styles.resultBoxTitle}>
+                                            <i className={"fa-regular fa-user " + styles.resultBoxTitle_icon}></i>Nhập Địa chỉ Email
+                                        </div>
+                                    </div>
+                                    <input onChange={props.nhap} placeholder="Email" autoFocus className={styles.resultBox_search} />
+                                    <div className={styles.listUserBox}>
+                                        {
+                                            props.listSearchUser.map((user, index) => <div key={index} className={styles.memberElementBox}>
+                                                <div className={styles.memberNameBox}>
+                                                    <div className={styles.avatarBox}>
+                                                        NH
+                                                    </div>
+                                                    <div className={styles.emailBox}>
+                                                        <div className={styles.name}>
+                                                            {user?.name}
+                                                        </div>
+                                                        <div className={styles.email}>
+                                                            {user?.email}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className={styles.buttonList}>
+                                                    <div className={styles.exitButton}>
+                                                        thêm
+                                                    </div>
+                                                </div>
+                                            </div>)
+                                        }
+                                    </div>
+
+                                    
+                                    <div className={styles.addButton}>
+                                        Thêm vào A.iSoft workspace
+                                    </div>
+                                </div> : <></>
+                            }
+                        </div>
+                    </div>
+                    <div className={styles.listMember}>
+                        {
+                            props.listUser.map((user, index) => <div key={index} className={styles.memberElementBox}>
+                                <div className={styles.memberNameBox}>
+                                    <div className={styles.avatarBox}>
+                                        NH
+                                    </div>
+                                    <div className={styles.emailBox}>
+                                        <div className={styles.name}>
+                                            {user?.name}
+                                        </div>
+                                        <div className={styles.email}>
+                                            {user?.email}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.buttonList}>
+                                    <div className={styles.exitButton}>
+                                        Rời đi
+                                    </div>
+                                </div>
+                            </div>)
+                        }
                     </div>
                 </div>
             </div>
